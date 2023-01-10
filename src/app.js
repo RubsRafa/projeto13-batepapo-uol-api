5a9d43cc8666519585dcbd5d64ddf70ea800cd43
RubsRafa <rubiarafaela2000@gmail.com>
2023-01-10 11:52:47 -0300

    post no participants

--- src/app.js
+++ src/app.js
@@ -8,3 +8,12 @@ app.use(express.json())
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Servidor funcionando na porta ${PORT}`))
+
+const participants = [];
+
+app.post('/participants', (req, res) => {
+    const { name } = req.body; 
+    if (!name || name === '') res.status(422).send('Name deve ser string não vazio')
+
+    participants.push(name)
+})
