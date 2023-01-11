07f8f6ef2ee02ded75df7d700033ac7f88c173e4
RubsRafa <rubiarafaela2000@gmail.com>
2023-01-11 14:02:31 -0300

    post no messages

--- src/app.js
+++ src/app.js
@@ -1,23 +1,31 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
 const app = express(); 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const PORT = 5000;
 
-app.listen(PORT, () => console.log(`Servidor funcionando na porta ${PORT}`))
+app.listen(PORT, () => console.log(`Servidor funcionando na porta ${PORT}`));
 
-const participants = [];
+const participants = []; //name, lastStatus, //salvo no Mongo > from, to, text, type, time; 
+const messages = []; //to, text, type; 
 
 app.post('/participants', (req, res) => {
-    const { name } = req.body; 
-    if (!name || name === '') res.status(422).send('Name deve ser string não vazio')
+    const user = req.body; 
+    if (!user || user.name === '') res.status(422).send('Name deve ser string não vazio')
 
-    participants.push(name)
-})
+    participants.push(user)
+    res.status(201).send('OK')
+});
 
 app.get('/participants', (req, res) => {
-    res.send(participants)
-})
+    res.send(participants);
+});
+
+app.post('/messages', (req, res) => {
+    const message = req.body; 
+    messages.push(message);
+    res.sendStatus(201);
+});
